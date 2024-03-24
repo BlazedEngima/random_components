8bc3cbb698003709a63f9a421bba8a5bbd0253c6
jose <meowdrey@hotmail.co.uk>
2024-03-24 19:31:25 +0700

    back to specific if with better variables

--- cmake/modules/FindMySQLCPPConnector.cmake
+++ cmake/modules/FindMySQLCPPConnector.cmake
@@ -4,6 +4,12 @@
 set(MySQLCPPConnector_ROOT_DIR "${CMAKE_SOURCE_DIR}/lib/mysql-cpp-connector")
 set(MySQLCPPConnector_INCLUDE_DIR "${MySQLCPPConnector_ROOT_DIR}/include")
 
+if(${CMAKE_SYSTEM_PROCESSOR} MATCHES "arm64" OR ${CMAKE_SYSTEM_PROCESSOR} MATCHES "aarch64")
+  set(MySQLCPPConnector_LIB_DIR "${MySQLCPPConnector_ROOT_DIR}/lib64")
+else()
+  set(MySQLCPPConnector_LIB_DIR "${MySQLCPPConnector_ROOT_DIR}/lib64-x86_64")
+endif()
+
 find_library(MySQLCPPConnector_LIB mysqlcppconn8
     HINTS
         ${MySQLCPPConnector_ROOT_DIR}/lib
