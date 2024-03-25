4d22a3f508393a182d7b301b99a10931406b4b63
jose <meowdrey@hotmail.co.uk>
2024-03-25 19:49:41 +0700

    changed transform function for Binance.

--- include/sym_handler.hpp
+++ include/sym_handler.hpp
@@ -10,8 +10,10 @@ namespace SymHandler
         static std::string transform(const std::string &symbol)
         {
             std::string lower_case = symbol;
+            lower_case.reserve(symbol.size() + 9);  // Reserve enough space for the original string, the underscore, and "_futures"
             std::transform(lower_case.begin(), lower_case.end(), lower_case.begin(), ::tolower);
             lower_case.insert(lower_case.find('u'), "_");
+            lower_case.append("_futures");
             return lower_case;
         }
     }
